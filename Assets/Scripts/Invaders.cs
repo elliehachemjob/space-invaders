77769d50a87db081c244086728202df6a99e888b
Ellie Hachem <elliehachemjob@gmail.com>
2024-05-11 16:22:37 +0200

    creating-grid-invaders-code-&-object

--- Assets/Scripts/Invaders.cs
+++ Assets/Scripts/Invaders.cs
@@ -2,7 +2,41 @@ using UnityEngine;
 
 public class Invaders : MonoBehaviour
 {
-    [Header("Invaders")]
+    public Invader[] prefabs = new Invader[5];
+    public int rows = 5;
+    public int columns = 11;
+
+
+    private void Awake()
+    {
+        CreateInvaderGrid();
+    }
+
+
+    private void CreateInvaderGrid()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            float width = 2f * (columns - 1);
+            float height = 2f * (rows - 1);
+
+            Vector2 centerOffset = new Vector2(-width * 0.5f, -height * 0.5f);
+            Vector3 rowPosition = new Vector3(centerOffset.x, (2f * i) + centerOffset.y, 0f);
+
+            for (int j = 0; j < columns; j++)
+            {
+                // Create a new invader and parent it to this transform
+                Invader invader = Instantiate(prefabs[i], transform);
+
+                // Calculate and set the position of the invader in the row
+                Vector3 position = rowPosition;
+                position.x += 2f * j;
+                invader.transform.localPosition = position;
+            }
+        }
+    }
+
+    /*  [Header("Invaders")]
     public Invader[] prefabs = new Invader[5];
     public AnimationCurve speed = new AnimationCurve();
 
@@ -152,5 +186,5 @@ public class Invaders : MonoBehaviour
 
         return count;
     }
-
+*/
 }
