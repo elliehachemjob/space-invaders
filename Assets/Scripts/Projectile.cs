ffd8cba47d57e952e1bbe03a8c44a976f99cd3f7
Ellie Hachem <elliehachemjob@gmail.com>
2024-05-17 13:27:29 +0200

    improving-projectile-code

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,49 +7,58 @@ public class Projectile : MonoBehaviour
 
     public Vector3 direction = Vector3.up;
     public float speed = 20f;
-
-    private void Update()
-    {
-        transform.position += speed * Time.deltaTime * direction;
-    }
-    private void OnTriggerStay2D(Collider2D other)
-    {
-        CheckCollision(other);
-    }
-
-    /* 
-    public Vector3 direction = Vector3.up;
-    public float speed = 20f;
-
     private new BoxCollider2D collider;
 
-    private void Awake()
-    {
-        collider = GetComponent<BoxCollider2D>();
-    }
-
     private void Update()
     {
         transform.position += speed * Time.deltaTime * direction;
     }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        CheckCollision(other);
-    }
-
     private void OnTriggerStay2D(Collider2D other)
     {
         CheckCollision(other);
     }
-
     private void CheckCollision(Collider2D other)
     {
         Bunker bunker = other.gameObject.GetComponent<Bunker>();
 
-        if (bunker == null || bunker.CheckCollision(collider, transform.position)) {
+        if (bunker == null || bunker.CheckCollision(collider, transform.position))
+        {
             Destroy(gameObject);
         }
-    }*/
+    }
+        /* 
+        public Vector3 direction = Vector3.up;
+        public float speed = 20f;
 
-}
+        private new BoxCollider2D collider;
+
+        private void Awake()
+        {
+            collider = GetComponent<BoxCollider2D>();
+        }
+
+        private void Update()
+        {
+            transform.position += speed * Time.deltaTime * direction;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            CheckCollision(other);
+        }
+
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            CheckCollision(other);
+        }
+
+        private void CheckCollision(Collider2D other)
+        {
+            Bunker bunker = other.gameObject.GetComponent<Bunker>();
+
+            if (bunker == null || bunker.CheckCollision(collider, transform.position)) {
+                Destroy(gameObject);
+            }
+        }*/
+
+    }
